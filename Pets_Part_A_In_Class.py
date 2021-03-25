89b111bc2f625b5c7d5dd8f2362ffa0a1f7381e1
abhikot1996 <79213991+abhikot1996@users.noreply.github.com>
2021-03-25 11:57:29 +0530

    Updated File

--- Pets_Part_A_In_Class.py
+++ Pets_Part_A_In_Class.py
@@ -33,9 +33,55 @@ class Pet:
     def setPlayful(self,play):
         self.playful = play
 
-Pet1 = Pet("Jim",3,False,True)
+    def __str__(self):
+        return (self.name + " is " + str(self.age) + " years old")
 
-print(Pet1.getName())
-print(Pet1.getPlayful())
-Pet1.setName("Snowball") # Setting name of pet
-print(Pet1.getName()) # Getting name of pet
+
+class Dog(Pet):
+    def __init__(self,name,age,hunger,playful,breed,FavoriteToy):
+        Pet.__init__(self,name,age,hunger,playful)
+        self.playful = playful
+        self.breed = breed
+        self.FavoriteToy = FavoriteToy
+    def wantsToPlay(self):
+        if self.playful == True:
+            return ("Dog wants to play with "+self.FavoriteToy)
+        else:
+            return ("Dog doesn't want to play")
+
+class Cat(Pet):
+    def __init__(self,name,age,hunger,playful,place):
+        Pet.__init__(self,name,age,hunger,playful)
+        self.playful = playful
+        self.FavoritePlaceToSit = place
+    def wantsToSit(self):
+        if self.playful == False:
+            print("The cat wants to sit in",self.FavoritePlaceToSit)
+        else:
+            print("The cat wants to play")
+    def __str__(self):
+        return (self.name + " likes to sit in "+self.FavoritePlaceToSit)
+
+
+huskyDog = Dog("Snowball",5,False,True,"Husky","Stick")
+Play = huskyDog.wantsToPlay()
+print(Play)
+
+huskyDog.playful = False
+Play = huskyDog.wantsToPlay()
+print(Play)
+
+typicalCat = Cat("Fluffy",3,False,False,"the sun ray")
+typicalCat.wantsToSit()
+
+print(typicalCat)
+
+
+
+
+# Pet1 = Pet("Jim",3,False,True)
+#
+# print(Pet1.getName())
+# print(Pet1.getPlayful())
+# Pet1.setName("Snowball") # Setting name of pet
+# print(Pet1.getName()) # Getting name of pet
