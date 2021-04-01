48829f395fd40775cc87ddea7dc504eceef28945
abhikot1996 <79213991+abhikot1996@users.noreply.github.com>
2021-04-01 11:25:33 +0530

    Added new file

---
+++ Math_Library.py
@@ -0,0 +1,22 @@
+
+import math
+
+Val = 3.14
+
+sqrtVal = math.sqrt(Val)
+print(sqrtVal)
+
+expVal = math.exp(Val)
+print(expVal)
+
+# factVal = math.factorial(Val)
+# print(factVal)
+
+sinVal = math.sin(Val)
+print(sinVal)
+
+floorVal = math.floor(Val)
+print(floorVal)
+
+ceilingVal = math.ceil(Val)
+print(ceilingVal)
