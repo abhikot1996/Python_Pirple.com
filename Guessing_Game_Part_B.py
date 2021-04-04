d2fdf86ae2ff237887ea4c7c93624448dd0d9c01
abhikot1996 <79213991+abhikot1996@users.noreply.github.com>
2021-04-04 10:53:14 +0530

    Added new files

---
+++ Guessing_Game_Part_B.py
@@ -0,0 +1,22 @@
+
+from random import random
+from time import clock
+randVal = random()
+print(randVal)
+
+upper = 1.0
+lower = 0.0
+# guess = 0.5
+startTime = clock()
+while True:
+    guess = (upper + lower) / 2
+    if guess == randVal:
+        break
+    elif guess < randVal:
+        lower = guess
+    else:
+        upper = guess
+
+endTime = clock()
+print(guess)
+print("It took us:",endTime - startTime,"seconds")
